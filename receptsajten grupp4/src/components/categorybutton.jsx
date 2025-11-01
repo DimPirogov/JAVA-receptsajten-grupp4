43216c83bd62e93c120f08eb45abb1327fe83b7e
yzx807652713 <140468185+yzx807652713@users.noreply.github.com>
2025-11-01 15:59:02 +0800

    update

--- receptsajten grupp4/src/components/categorybutton.jsx
+++ receptsajten grupp4/src/components/categorybutton.jsx
@@ -1,25 +1,31 @@
-import React from "react";
-import { useNavigate } from "react-router-dom"; // for page navigation
+import React, { useState } from "react";
+import { useNavigate, useLocation, useParams } from "react-router-dom"; // for page navigation
 import "./categorybutton.css";
-
 // Category button component
 export default function CategoryButton({ name, isActive }) {
   const navigate = useNavigate();
-
+  const [bold, setBold] = useState()
+  const location = useLocation();
+  const path = location.pathname; // "/category/tequila"
+  const category = path.split("/")[2]; // ✅ "tequila"
   // When the button is clicked, go to the category page
   const handleClick = () => {
     // convert category name into lowercase (like "gin", "rum", "vodka")
     const categoryId = name.toLowerCase().replace("drinkar", "");
+    console.log(categoryId);
+    setBold(categoryId)
+
     if (isActive) {
       navigate("/");
-    }else {
+    } else {
       navigate(`/category/${categoryId}`);
     }
   };
 
+
   return (
     <button
-      className={`categorybutton ${isActive ? "active" : ""}`}
+      className={`categorybutton ${isActive ? "active" : ""} ${bold===category?'bold':''}`}
       onClick={handleClick}
     >
       {name}

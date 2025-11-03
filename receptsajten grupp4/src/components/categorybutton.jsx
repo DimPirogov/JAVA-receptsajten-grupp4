0467099decaf436984c92cd864ec0e6a4898bee4
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-03 22:37:03 +0100

    fixed togglefunction

--- receptsajten grupp4/src/components/categorybutton.jsx
+++ receptsajten grupp4/src/components/categorybutton.jsx
@@ -3,17 +3,21 @@ import { useNavigate } from "react-router-dom"; // for page navigation
 import "./categorybutton.css";
 
 // Category button component
-export default function CategoryButton({ name, isActive, count }) {
+export default function CategoryButton({ name, isActive, count, onClick }) {
   const navigate = useNavigate();
 
   // When the button is clicked, go to the category page
   const handleClick = () => {
-    // convert category name into lowercase (like "gin", "rum", "vodka")
-    const categoryId = name.toLowerCase().replace(/\s*drinkar$/i, "").replace(/\s+/g, "-");
-    if (isActive) {
-      navigate("/");
-    }else {
-      navigate(`/category/${categoryId}`);
+
+    if (onClick) {
+      onClick();
+    } else {
+      const categoryId = name.toLowerCase().replace(/\s*drinkar$/i, "").replace(/\s+/g, "-");
+      if (isActive) {
+        navigate("/");
+      } else {
+        navigate(`/category/${categoryId}`);
+      }
     }
   };
 
